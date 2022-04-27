0970b12201ea21e18eb696980a61f100308ea6f9
elducati <geomondi09@gmail.com>
2022-04-27 19:41:57 +0300

    initial commit

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -38,20 +38,20 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   // paste in your front-end address here to get 10 balloons on deploy:
   await balloons.transfer(
-    "0x08C01CEc8B8c793D768f502b604113074CE212aD",
+    "0x22FECa197670B1EA09AB918A5D5954315Ff42bE7",
     "" + 10 * 10 ** 18
   );
 
-  // // uncomment to init DEX on deploy:
-  // console.log(
-  //   "Approving DEX (" + dex.address + ") to take Balloons from main account..."
-  // );
-  // // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address, ethers.utils.parseEther("100"));
-  // console.log("INIT exchange...");
-  // await dex.init(ethers.utils.parseEther("5"), {
-  //   value: ethers.utils.parseEther("5"),
-  //   gasLimit: 200000,
-  // });
+  // uncomment to init DEX on deploy:
+  console.log(
+    "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+  );
+  // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dex.address, ethers.utils.parseEther("0.001"));
+  console.log("INIT exchange...");
+  await dex.init(ethers.utils.parseEther("0.0001"), {
+    value: ethers.utils.parseEther("0.0001"),
+    gasLimit: 200000,
+  });
 };
 module.exports.tags = ["Balloons", "DEX"];
